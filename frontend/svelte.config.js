7c877563c60de834e1cfbf1e8534749f75ceb40a
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-07-07 22:22:35 +0200

    fix(frontend): fix build error when API is absent

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,11 +3,22 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ path, message }) => {
+        // ignore unavailable API endpoint during prerender
+        if (path === '/status') {
+          return;
+        }
+
+        // otherwise fail the build
+        throw new Error(message);
+      }
+    }
+  },
 };
 
 export default config;
