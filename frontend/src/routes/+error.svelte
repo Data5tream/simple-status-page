b0803b844428a5ee9ee9a97744ce0895a38b3f35
Data5tream <6423657+Data5tream@users.noreply.github.com>
2024-03-31 19:32:56 +0200

    chore(frontend): update node and dependencies

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import '$lib/main.css';
+  import PageFooter from "$lib/components/PageFooter.svelte";
+  import PageHeader from "$lib/components/PageHeader.svelte";
+</script>
+
+<main>
+    <PageHeader />
+        <span class="error">Error loading status data :(</span>
+</main>
+<PageFooter />
+
+<style lang="scss">
+  .error {
+    display: block;
+    color: var(--c-red);
+    font-size: 64px;
+  }
+</style>
