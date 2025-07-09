2b21e841fec340ac04f3ba723e6a9c99e97e3e6f
Data5tream <6423657+Data5tream@users.noreply.github.com>
2025-07-09 20:10:54 +0200

    Rewrite frontend in angular

---
+++ frontend/src/app/data-service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, switchMap, timer } from 'rxjs';
+
+export interface Watchpoint {
+  status: number;
+  watchpoint: {
+    id: string;
+    name: string;
+    kind: "url" | "keyword";
+    target: string;
+    keyword?: string;
+  };
+}
+
+export interface StatusData {
+  watchpoints?: Array<Watchpoint>;
+}
+
+export async function loadStatus(): Promise<StatusData> {
+  try {
+    // TODO: replace with correct URL
+    const result = await fetch("http:/127.0.0.1:8000/status");
+    if (result.status !== 200) {
+      return Promise.reject(new Error("Fetch error"));
+    }
+
+    const watchpoints = await result.json();
+    return { watchpoints };
+  } catch (_) {
+    return Promise.reject(new Error("Unknown error"));
+  }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private url = "http://127.0.0.1:8000/status";
+
+  constructor(private http: HttpClient) { }
+
+  fetchData(): Observable<Array<Watchpoint>> {
+    return timer(0, 2000).pipe(switchMap(() => this.http.get<Array<Watchpoint>>(this.url)));
+  }
+}
+
---
+++ frontend/src/status/status-entry.ts
@@ -0,0 +1,40 @@
+import { Component, computed, input } from "@angular/core";
+
+import { Watchpoint } from "../app/data-service";
+import { StatusIcon } from "./status-icon";
+
+@Component({
+  selector: "status-entry",
+  imports: [StatusIcon],
+  templateUrl: "./status-entry.html",
+  styleUrl: "./status-entry.scss",
+})
+export class StatusEntry {
+  watchpoint = input.required<Watchpoint>();
+
+  statusMsg = computed(() => {
+    const data = this.watchpoint();
+
+    if (!data) {
+      return "";
+    }
+
+    if (data.status === 200) {
+      return 'OK (200)';
+    } else if (data.status === 404) {
+      return `Not found (${data.status})`;
+    } else if (data.status < 600) {
+      return `${data.status}`;
+    } else if (data.status === 600) {
+      return 'Invalid DNS';
+    } else if (data.status === 601) {
+      return 'TLS validation error';
+    } else if (data.status === 604) {
+      return 'Keyword not found';
+    } else if (data.status === 610) {
+      return 'Unable to parse HTML content';
+    }
+
+    return "Unkown status";
+  });
+}
